ea01c79e2f3089d363a62989d7f146878cd7379c
Guilherme Gomes De Oliveira <gugomes-@c1r4p3.42sp.org.br>
2024-12-13 14:37:50 -0300

    Player rendering fixed

--- src/load_img.c
+++ src/load_img.c
@@ -6,7 +6,7 @@
 /*   By: gugomes- <gugomes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 12:53:20 by gugomes-          #+#    #+#             */
-/*   Updated: 2024/12/09 16:17:41 by gugomes-         ###   ########.fr       */
+/*   Updated: 2024/12/13 14:36:46 by gugomes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 int load_images(t_game *game)
 {
-    game->player_image = mlx_xpm_file_to_image(game->mlx, "./assets/player/cell.xpm", &game->x_pos, &game->y_pos);
-    game->wall_image = mlx_xpm_file_to_image(game->mlx, "./assets/tiles/wall.xpm", &game->x_pos, &game->y_pos);
-    game->path_image = mlx_xpm_file_to_image(game->mlx, "./assets/tiles/path.xpm", &game->x_pos, &game->y_pos);
-    game->exit_image = mlx_xpm_file_to_image(game->mlx, "./assets/tiles/vein.xpm", &game->x_pos, &game->y_pos);
-    game->collectable_image = mlx_xpm_file_to_image(game->mlx, "./assets/tiles/o2.xpm", &game->x_pos, &game->y_pos);
+    int widht;
+    int height;
+    
+    game->player_image = mlx_xpm_file_to_image(game->mlx, "./img/P.xpm", &widht, &height);
+    game->wall_image = mlx_xpm_file_to_image(game->mlx, "./img/1.xpm", &widht, &height);
+    game->path_image = mlx_xpm_file_to_image(game->mlx, "./img/0.xpm", &widht, &height);
+    game->exit_image = mlx_xpm_file_to_image(game->mlx, "./img/E.xpm", &widht, &height);
+    game->collectable_image = mlx_xpm_file_to_image(game->mlx, "./img/C.xpm", &widht, &height);
 
     if (!game->player_image || !game->wall_image || !game->path_image ||
         !game->exit_image || !game->collectable_image)
--- src/map_set.c
+++ src/map_set.c
@@ -6,7 +6,7 @@
 /*   By: gugomes- <gugomes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 14:07:33 by gugomes-          #+#    #+#             */
-/*   Updated: 2024/12/10 16:15:36 by gugomes-         ###   ########.fr       */
+/*   Updated: 2024/12/13 14:31:07 by gugomes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	**parse_map(t_game *game, int argc, char **argv)
 	}
 	check_if_map_file_is_ber(argv[1]);
 
-	game->file_path = ft_strjoin("./assets/map/", argv[1]);
+	game->file_path = ft_strjoin("./map/", argv[1]);
 	trace_map(game);
 	load_map(game);
 
