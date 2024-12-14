32c2f7a40905d24c8b1acdbc69efc972813bfd6c
Guilherme Gomes De Oliveira <gugomes-@c1r4p3.42sp.org.br>
2024-12-14 02:18:05 -0300

    finished

--- includes/game.h
+++ includes/game.h
@@ -6,7 +6,7 @@
 /*   By: gugomes- <gugomes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 15:36:31 by gugomes-          #+#    #+#             */
-/*   Updated: 2024/12/14 00:40:46 by gugomes-         ###   ########.fr       */
+/*   Updated: 2024/12/14 02:15:03 by gugomes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,43 +23,44 @@
 
 # define SZ 32
 
-typedef struct s_game {
-    void    *mlx;
-    void    *win;
-    void    *p_img;
-    void    *o_img;
-    void    *z_img;
-    void    *e_img;
-    void    *c_img;
+typedef struct s_game
+{
+	void	*mlx;
+	void	*win;
+	void	*p_img;
+	void	*o_img;
+	void	*z_img;
+	void	*e_img;
+	void	*c_img;
 	int		e_count;
 	int		p_count;
 	int		c_count;
-    int     x_pos;
-    int     y_pos;
-    int     collectables;
-    int     map_height;
-    int     map_width;
-    char    **map;
-    char    *file_path;
-} t_game;
+	int		x_pos;
+	int		y_pos;
+	int		collectables;
+	int		map_height;
+	int		map_width;
+	char	**map;
+	char	*file_path;
+}	t_game;
 
-void    load_map(t_game *game);
+void	load_map(t_game *game);
 void	free_images(t_game *game);
 void	free_map(char **map, int height);
-void    print_map(t_game *game);
-int     key_press(int keycode, t_game *game);
-int     close_game(t_game *game);
-int     render_player(t_game *game);
-int     render_map(t_game *game);
-int     load_images(t_game *game);
-int     check_if_map_file_is_ber(char *argv);
-int     count_digits(int n);
+void	print_map(t_game *game);
+int		key_press(int keycode, t_game *game);
+int		close_game(t_game *game);
+int		render_player(t_game *game);
+int		render_map(t_game *game);
+int		load_images(t_game *game);
+int		check_if_map_file_is_ber(char *argv);
+int		count_digits(int n);
 int		ft_putstr_fd_int(const char *s, int fd);
 int		init_game(t_game *game, int argc, char **argv);
-char    **parse_map(t_game *game, int argc, char **argv);
-void    trace_map(t_game *game);
+char	**parse_map(t_game *game, int argc, char **argv);
+void	trace_map(t_game *game);
 void	count_elements(t_game *game);
 int		validate_map(char **map, t_game *game);
-void debug_map(char **map);
+void	debug_map(char **map);
 
 #endif
--- src/map_verification.c
+++ src/map_verification.c
@@ -6,101 +6,108 @@
 /*   By: gugomes- <gugomes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 23:30:14 by gugomes-          #+#    #+#             */
-/*   Updated: 2024/12/14 00:58:10 by gugomes-         ###   ########.fr       */
+/*   Updated: 2024/12/14 02:12:32 by gugomes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/game.h"
 
-static int validate_character(t_game *game, char c)
+static int	validate_character(t_game *game, char c)
 {
-    if (c == 'P')
-    {
-        if (game->p_count > 1)
-            return (ft_putstr_fd_int("Error: Too many players on the map!\n", 2));
-    }
-    else if (c == 'E')
-    {
-        if (game->e_count > 1)
-            return (ft_putstr_fd_int("Error: Too many exits on the map!\n", 2));
-    }
-    else if (c == 'C')
-        game->c_count++;
-    else if (!ft_strchr("PCE01\n", c))
-        return (ft_putstr_fd_int("Error: Invalid map element!\n", 2));
-    return (1);
+	if (c == 'P')
+	{
+		if (game->p_count > 1)
+			return (ft_putstr_fd_int("Error: \
+			 Too many players on the map!\n", 2));
+	}
+	else if (c == 'E')
+	{
+		if (game->e_count > 1)
+			return (ft_putstr_fd_int("Error: Too many exits on the map!\n", 2));
+	}
+	else if (c == 'C')
+		game->c_count++;
+	else if (!ft_strchr("PCE01\n", c))
+		return (ft_putstr_fd_int("Error: Invalid map element!\n", 2));
+	return (1);
 }
 
-static int validate_elements(char **map, t_game *game)
+static int	validate_elements(char **map, t_game *game)
 {
-    int row = 0;
-    int col;
+	int	row;
+	int	col;
 
-    while (map[row])
-    {
-        col = 0;
-        while (map[row][col])
-        {
-            if (!validate_character(game, map[row][col]))
-                return (0);
-            col++;
-        }
-        row++;
-    }
-    if (game->p_count == 0)
-        return (ft_putstr_fd_int("Error: No player on the map.\n", 2));
-    if (game->c_count == 0)
-        return (ft_putstr_fd_int("Error: No collectibles on the map.\n", 2));
-    if (game->e_count == 0)
-        return (ft_putstr_fd_int("Error: No exit on the map.\n", 2));
-    return (1);
+	row = 0;
+	while (map[row])
+	{
+		col = 0;
+		while (map[row][col])
+		{
+			if (!validate_character(game, map[row][col]))
+				return (0);
+			col++;
+		}
+		row++;
+	}
+	if (game->p_count == 0)
+		return (ft_putstr_fd_int("Error: No player on the map.\n", 2));
+	if (game->c_count == 0)
+		return (ft_putstr_fd_int("Error: No collectibles on the map.\n", 2));
+	if (game->e_count == 0)
+		return (ft_putstr_fd_int("Error: No exit on the map.\n", 2));
+	return (1);
 }
 
-static int validate_walls(char **map)
+static int	validate_walls(char **map)
 {
-    size_t row = 0;
-    size_t col;
-    size_t width = ft_strlen(map[0]);
+	size_t	row;
+	size_t	width;
+	size_t	col;
 
-    while (map[row])
-    {
-        if (row == 0 || map[row + 1] == NULL)
-        {
-            for (col = 0; col < width; col++)
-                if (map[row][col] != '1')
-                    return (ft_putstr_fd_int("Error: Map borders must be walls (1).\n", 2));
-        }
-        else
-        {
-            if (map[row][0] != '1' || map[row][width - 1] != '1')
-                return (ft_putstr_fd_int("Error: Map borders must be walls (1).\n", 2));
-        }
-        row++;
-    }
-    return (1);
+	col = 0;
+	width = ft_strlen(map[0]);
+	row = 0;
+	while (map[row])
+	{
+		if (row == 0 || map[row + 1] == NULL)
+		{
+			while (col < width)
+			{
+				if (map[row][col] != '1')
+					return (ft_putstr_fd_int("Error\nMap wall error\n", 2));
+				col++;
+			}
+		}
+		else if (map[row][0] != '1' || map[row][width - 1] != '1')
+			return (ft_putstr_fd_int("Error\nMap wall error\n", 2));
+		row++;
+	}
+	return (1);
 }
 
-static int is_map_rectangular(char **map)
+static int	is_map_rectangular(char **map)
 {
-    size_t width = ft_strlen(map[0]);
-    size_t row = 1;
+	size_t	width;
+	size_t	row;
 
-    while (map[row])
-    {
-        if (ft_strlen(map[row]) != width)
-            return (ft_putstr_fd_int("Error: The map is not rectangular.\n", 2));
-        row++;
-    }
-    return (1);
+	width = ft_strlen(map[0]);
+	row = 1;
+	while (map[row])
+	{
+		if (ft_strlen(map[row]) != width)
+			return (ft_putstr_fd_int("Error: map not rectangular.\n", 2));
+		row++;
+	}
+	return (1);
 }
 
-int validate_map(char **map, t_game *game)
+int	validate_map(char **map, t_game *game)
 {
-    if (!is_map_rectangular(map))
-        return (0);
-    if (!validate_walls(map))
-        return (0);
-    if (!validate_elements(map, game))
-        return (0);
-    return (1);
+	if (!is_map_rectangular(map))
+		return (0);
+	if (!validate_walls(map))
+		return (0);
+	if (!validate_elements(map, game))
+		return (0);
+	return (1);
 }
