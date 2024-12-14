80d2bb9c9e8956db6735a3dda792d31ddff8dbdb
Guilherme Gomes De Oliveira <gugomes-@c1r4p3.42sp.org.br>
2024-12-14 01:55:41 -0300

    norminette_v3

--- src/render.c
+++ src/render.c
@@ -6,58 +6,68 @@
 /*   By: gugomes- <gugomes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 15:30:42 by gugomes-          #+#    #+#             */
-/*   Updated: 2024/12/14 00:10:06 by gugomes-         ###   ########.fr       */
+/*   Updated: 2024/12/14 01:55:01 by gugomes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/game.h"
 
-int render_player(t_game *game)
+int	render_player(t_game *game)
 {
-    mlx_put_image_to_window(game->mlx, game->win, game->p_img, \
-         game->x_pos, game->y_pos);
-    return (0);
+	mlx_put_image_to_window(game->mlx, game->win, game->p_img, \
+		game->x_pos, game->y_pos);
+	return (0);
 }
 
-int render_map(t_game *game)
+static void	put_image_for_cell(t_game *game, char cell, int x, int y)
 {
-    int x;
-    int y;
-
-    y = 0;
-    while (y < game->map_height )
-    {
-        x = 0;
-        while (x < game->map_width)
-        {
-            if (game->map[y][x] == '1')
-                mlx_put_image_to_window(game->mlx, game->win, game->o_img, x * SZ, y * SZ);
-            else if (game->map[y][x] == '0')
-                mlx_put_image_to_window(game->mlx, game->win, game->z_img, x * SZ, y * SZ);
-            else if (game->map[y][x] == 'C')
-                mlx_put_image_to_window(game->mlx, game->win, game->c_img, x * SZ, y * SZ);
-            else if (game->map[y][x] == 'E')
-                mlx_put_image_to_window(game->mlx, game->win, game->e_img, x * SZ, y * SZ);
-            x++;
-        }
-        y++;
-    }
-    return (0);
+	if (cell == '1')
+		mlx_put_image_to_window(game->mlx, game->win, game->o_img, \
+			x * SZ, y * SZ);
+	else if (cell == '0')
+		mlx_put_image_to_window(game->mlx, game->win, game->z_img, \
+			x * SZ, y * SZ);
+	else if (cell == 'C')
+		mlx_put_image_to_window(game->mlx, game->win, game->c_img, \
+			x * SZ, y * SZ);
+	else if (cell == 'E')
+		mlx_put_image_to_window(game->mlx, game->win, game->e_img, \
+			x * SZ, y * SZ);
 }
 
-int load_images(t_game *game)
+int	render_map(t_game *game)
 {
-    int wdt;
-    int hgt;
-    
-    game->p_img = mlx_xpm_file_to_image(game->mlx, "./img/P.xpm", &wdt, &hgt);
-    game->o_img = mlx_xpm_file_to_image(game->mlx, "./img/1.xpm", &wdt, &hgt);
-    game->z_img = mlx_xpm_file_to_image(game->mlx, "./img/0.xpm", &wdt, &hgt);
-    game->e_img = mlx_xpm_file_to_image(game->mlx, "./img/E.xpm", &wdt, &hgt);
-    game->c_img = mlx_xpm_file_to_image(game->mlx, "./img/C.xpm", &wdt, &hgt);
+	int		x;
+	int		y;
+	char	cell;
 
-    if (!game->p_img || !game->o_img || !game->z_img ||
-        !game->e_img || !game->c_img)
-        return (ft_putstr_fd("Image loading error.", 2), -1);
-    return (0);
+	y = 0;
+	while (y < game->map_height)
+	{
+		x = 0;
+		while (x < game->map_width)
+		{
+			cell = game->map[y][x];
+			put_image_for_cell(game, cell, x, y);
+			x++;
+		}
+		y++;
+	}
+	return (0);
+}
+
+int	load_images(t_game *game)
+{
+	int	wdt;
+	int	hgt;
+
+	game->p_img = mlx_xpm_file_to_image(game->mlx, "./img/P.xpm", &wdt, &hgt);
+	game->o_img = mlx_xpm_file_to_image(game->mlx, "./img/1.xpm", &wdt, &hgt);
+	game->z_img = mlx_xpm_file_to_image(game->mlx, "./img/0.xpm", &wdt, &hgt);
+	game->e_img = mlx_xpm_file_to_image(game->mlx, "./img/E.xpm", &wdt, &hgt);
+	game->c_img = mlx_xpm_file_to_image(game->mlx, "./img/C.xpm", &wdt, &hgt);
+	if (!game->p_img || !game->o_img || !game->z_img || \
+		!game->e_img || !game->c_img)
+		return (ft_putstr_fd("Image loading error.", 2), -1);
+	return (0);
 }
--- src/verify_map.c
+++ src/verify_map.c
@@ -6,29 +6,28 @@
 /*   By: gugomes- <gugomes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 17:40:36 by gugomes-          #+#    #+#             */
-/*   Updated: 2024/12/13 17:13:43 by gugomes-         ###   ########.fr       */
+/*   Updated: 2024/12/14 01:49:20 by gugomes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/game.h"
 
-void print_map(t_game *game)
+void	print_map(t_game *game)
 {
-	int 	x;
-	int 	y;
+	int	x;
+	int	y;
 
 	x = 0;
 	y = 0;
-
 	while (y <= game->map_height - 1)
 	{
 		x = 0;
 		while (x <= game->map_width)
 		{
-			printf("%c", game->map[y][x]);
+			write(1, &game->map[y][x], 1);
 			x++;
 		}
-		printf("\n");
+		write(1, "\n", 1);
 		y++;
 	}
 }
